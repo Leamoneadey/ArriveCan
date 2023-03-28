277b0796bbdc43587c0c847f3de99c3f9d674cbc
Leamoneadey <sany.velomanana@gmail.com>
2023-03-28 17:43:20 -0400

    add StartPage

---
+++ app/src/main/java/com/example/arrivecan/Reason.java
@@ -0,0 +1,14 @@
+package com.example.arrivecan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reason extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reason);
+    }
+}
