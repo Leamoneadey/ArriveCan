382414a5667e3b67d8eab970e82e693b0a62402b
kewiille <kerlle_teguia@hotmail.com>
2023-03-28 20:05:08 -0400

    changes java

--- app/src/main/java/com/example/arrivecan/MainActivity.java
+++ app/src/main/java/com/example/arrivecan/MainActivity.java
@@ -2,18 +2,32 @@ package com.example.arrivecan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences name,pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity1_main);
+
+        name = getPreferences(Context.MODE_PRIVATE);
+        pass = getPreferences(Context.MODE_PRIVATE);
+
+        String ch = name.getString("leName", "rien");
+        String nb = pass.getString("MDP", "rien");
+
+        ((TextView)findViewById(R.id.editTextTextEmailAddress)).setText(ch);
+        ((TextView)findViewById(R.id.editTextNumberPassword)).setText(nb);
     }
 
-    public <View> void clickEnter(View view){
+    public void clickEnter(View v){
         startActivity(new Intent(getApplicationContext(), GetStarted.class));
     }
 }
